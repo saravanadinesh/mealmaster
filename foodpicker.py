919c032cad6f1ef0bbbd10e0bd1bc2d53fe9b7ab
Vishnu Kumar <vishnukumar.gr86@gmail.com>
2023-05-26 16:41:40 +0530

    Updated host IP for the server

--- foodpicker.py
+++ foodpicker.py
@@ -737,4 +737,4 @@ def planmeals(diet="vegetarian", planfor="day", day="day", mealtime="lunch", req
 # mealplan = planmeals(planfor="day")
 # print(json.dumps(mealplan, indent=4))
 if __name__ == '__main__':
-    app.run()
+    app.run('0.0.0.0', port=5000)
